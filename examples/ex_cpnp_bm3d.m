3d0b7e59cc6cb1b7daf7f685e0515b01ce5f6e06
ChaoTang0330 <happy.tang.chao@outlook.com>
2024-02-11 02:46:16 +0000

    Add example scripts

---
+++ examples/ex_cpnp_bm3d.m
@@ -0,0 +1,10 @@
+clear 
+clc
+
+config = ['.', filesep, 'config', filesep, 'airi_sim.json'];
+msFile = ['.', filesep, 'simulated_measurements', filesep, 'dt8', filesep, '3c353_lrs_1.0_seed_0.mat'];
+resultPath = ['.', filesep, 'results', filesep, '3c353_dt8_seed0', filesep, 'cPnP-BM3D']; 
+algorithm = 'cpnp-bm3d';
+RunID = 0;
+
+run_imager(config, 'msFile', msFile, 'algorithm', algorithm, 'resultPath', resultPath, 'runID', 0)
