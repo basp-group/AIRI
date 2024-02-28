fd4736e2200657774e446d1ada7130d4d4ee3e01
ChaoTang0330 <happy.tang.chao@outlook.com>
2024-02-28 23:51:14 +0000

    Add final output

--- imager.m
+++ imager.m
@@ -112,6 +112,14 @@ function imager(pathData, imPixelSize, imDimx, imDimy, param_general, runID)
     fitswrite(RESULTS.MODEL, fullfile(param_imaging.resultPath, [param_algo.algorithm, '_model_image.fits']))
     fitswrite(RESULTS.RESIDUAL, fullfile(param_imaging.resultPath, [param_algo.algorithm, '_residual_dirty_image.fits']))
     fitswrite(RESULTS.RESIDUAL ./ PSFPeak, fullfile(param_imaging.resultPath, [param_algo.algorithm, '_residual_dirty_image_normalised.fits']))
+
+    %% Final metrics
+    fprintf('\nINFO: The std of the residual dirty image %g', std(RESULTS.RESIDUAL, 0, 'all'))
+    try
+        gdth_img = fitsread(param_imaging.groundtruth);
+        rsnr = 20*log10( norm(gdth_img(:)) / norm(RESULTS.MODEL(:) - gdth_img(:)) );
+        fprintf('\nINFO: The signal-to-noise ratio of the reconstructed image %f dB', rsnr)
+    end
     
     fprintf('\nTHE END\n')
     end
