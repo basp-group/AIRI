6e268d15fc845d8138da84ebe47402b4779d4db8
ChaoTang0330 <happy.tang.chao@outlook.com>
2024-02-28 15:14:45 +0000

    Update example

--- examples/ex_cairi_oaid.m
+++ examples/ex_cairi_oaid.m
@@ -4,10 +4,10 @@ clc
 cd ..
 
 config = ['.', filesep, 'config', filesep, 'airi_sim.json'];
-msFile = ['.', filesep, 'examples', filesep, 'simulated_measurements', filesep, 'dt8', filesep, '3c353_lrs_1.0_seed_0.mat'];
+dataFile = ['.', filesep, 'examples', filesep, 'simulated_measurements', filesep, 'dt8', filesep, '3c353_lrs_1.0_seed_0.mat'];
 resultPath = ['.', filesep, 'results', filesep, '3c353_dt8_seed0', filesep, 'cAIRI-OAID']; 
 algorithm = 'cairi';
 shelf_pth = ['.', filesep, 'airi_denoisers', filesep, 'shelf_oaid.csv'];
 RunID = 0;
 
-run_imager(config, 'msFile', msFile, 'algorithm', algorithm, 'resultPath', resultPath, 'dnnShelfPath', shelf_pth, 'runID', 0)
+run_imager(config, 'dataFile', dataFile, 'algorithm', algorithm, 'resultPath', resultPath, 'dnnShelfPath', shelf_pth, 'runID', 0)
--- examples/ex_upnp_bm3d.m
+++ examples/ex_upnp_bm3d.m
@@ -4,9 +4,9 @@ clc
 cd ..
 
 config = ['.', filesep, 'config', filesep, 'airi_sim.json'];
-msFile = ['.', filesep, 'examples', filesep, 'simulated_measurements', filesep, 'dt8', filesep, '3c353_lrs_1.0_seed_0.mat'];
+dataFile = ['.', filesep, 'examples', filesep, 'simulated_measurements', filesep, 'dt8', filesep, '3c353_lrs_1.0_seed_0.mat'];
 resultPath = ['.', filesep, 'results', filesep, '3c353_dt8_seed0', filesep, 'uPnP-BM3D']; 
 algorithm = 'upnp-bm3d';
 RunID = 0;
 
-run_imager(config, 'msFile', msFile, 'algorithm', algorithm, 'resultPath', resultPath, 'runID', 0)
+run_imager(config, 'dataFile', dataFile, 'algorithm', algorithm, 'resultPath', resultPath, 'runID', 0)
